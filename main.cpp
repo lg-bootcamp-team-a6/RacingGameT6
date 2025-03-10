a2c9facf4a62ec95d38501254e9d4765e0699408
sangjunpark97 <djg04138@gmail.com>
2025-03-10 17:14:30 +0900

    test

--- main.cpp
+++ main.cpp
@@ -9,12 +9,13 @@ int main(int argc, char** argv)
     QApplication a(argc,argv);
 
     InputDeviceHandler inputHandler;
+    
     // 50ms마다 input events를 처리
-    QTimer timer;
+    /*QTimer timer;
     QObject::connect(&timer, &QTimer::timeout, &inputHandler, &InputDeviceHandler::processInputEvents);
     timer.start(50);  // 50ms마다 처리
 
-    qDebug() << "TESTESTESTESTTEST";
+    qDebug() << "TESTESTESTESTTEST";*/
 
     View v;
     v.show();
