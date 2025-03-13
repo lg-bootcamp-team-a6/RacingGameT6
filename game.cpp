9a6f083f0496767af12a4d5be61cf7822e3d8aab
sangjunpark97 <djg04138@gmail.com>
2025-03-13 11:15:55 +0900

    resource update

--- game.cpp
+++ game.cpp
@@ -73,9 +73,6 @@ void Game::resetGameData(int mapIdx)
         offsetY = 0;
         car[0].x = 220 * gamescale;
         car[0].y = 1500 * gamescale;
-        m_pixelRed = 200;
-        m_pixelGreen = 300;
-        m_pixelGreen = 300;
         qDebug() << "Game data reset for map 0";
     }
     else if (mapIdx == 1)
@@ -91,9 +88,6 @@ void Game::resetGameData(int mapIdx)
         offsetY = 0;
         car[0].x = 250 * gamescale;
         car[0].y = 1300 * gamescale;
-        m_pixelRed = 200;
-        m_pixelGreen = 300;
-        m_pixelGreen = 300;
         qDebug() << "Game data reset for map 1";
     }
     else if (mapIdx == 2)
@@ -109,9 +103,6 @@ void Game::resetGameData(int mapIdx)
         offsetY = 0;
         car[0].x = 290 * gamescale;
         car[0].y = 620 * gamescale;
-        m_pixelRed = 200;
-        m_pixelGreen = 300;
-        m_pixelGreen = 300;
         qDebug() << "Game data reset for map 1";
     }
     else
