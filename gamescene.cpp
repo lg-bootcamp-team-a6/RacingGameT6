465d15b17ef7c7717bac91e2cc13f4089116b5dd
Soyeon11 <nuna1016@gmail.com>
2025-03-17 11:12:42 +0900

    Add send image for map status

--- gamescene.cpp
+++ gamescene.cpp
@@ -28,7 +28,12 @@ GameScene::GameScene(QObject *parent)
     m_timer->setInterval(m_game.ITERATION_VALUE);
     connect(m_timer, &QTimer::timeout, this, &GameScene::update);
     m_timer->start(m_game.ITERATION_VALUE);
-
+    //Send map status
+    
+    char str[20]; // 문자열 크기 20 (64bit + NULL 종료자)
+    sprintf(str, "%d", m_mapIdx);  // 숫자를 문자열로 변환
+    qDebug() << "Send map status" << str;
+    m_pUdpSocketHandler -> BtHsendMessage(MAP_STATUS, str);
     update();
 }
 
@@ -566,6 +571,10 @@ void GameScene::setMapIdx(int mapIdx)
     m_elapsedTime = 0;
     update();
     Wait3Seconds();
+    char str[20]; // 문자열 크기 20 (64bit + NULL 종료자)
+    sprintf(str, "%d", m_mapIdx);  // 숫자를 문자열로 변환
+    qDebug() << "Send map status in changed mode" << str;
+    m_pUdpSocketHandler -> BtHsendMessage(MAP_STATUS, str);
 }
 
 bool GameScene::checkStarCollision()
@@ -608,7 +617,7 @@ void GameScene::Goal()
 
     m_game.m_rankRecord[m_mapIdx].append(m_elapsedTime);
     std::sort(m_game.m_rankRecord[m_mapIdx].begin(), m_game.m_rankRecord[m_mapIdx].end());
-
+    //Send elapsedTime
     int seconds = m_elapsedTime / 100;
     int mseconds = m_elapsedTime % 100;
 
