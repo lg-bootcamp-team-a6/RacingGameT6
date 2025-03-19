0bff83acbf5ba75de99afafb63e95236025e58cc
sangjunpark97 <djg04138@gmail.com>
2025-03-19 11:31:05 +0900

    reset 최대속도 제어 수정

--- view.cpp
+++ view.cpp
@@ -23,7 +23,6 @@ View::View()
 }
 
 void View::resetBotton() {
-    m_gameScene->m_game.maxSpeed = 0;
     m_accelForwardButton->setEnabled(true);
     m_accelBackButton->setEnabled(true);
     m_brakeButton->setEnabled(true);
